171a75816450cecdddf9aa4b069c748645f3cbed
Isaque Hoffmeister <isaque.ha@gmail.com>
2020-09-02 11:04:23 -0300

    feat: placeholder for simplebar

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import Header from '../Header';
 import Footer from '../Footer';
+// import SimpleBar from 'simplebar-react';
+// import 'simplebar-react/dist/simplebar.min.css';
 
 const Main = styled.main`
   background-color: var(--black);
@@ -20,7 +22,9 @@ function PageDefault({ children, paddingAll }) {
     <>
       <Header />
       <Main paddingAll={paddingAll}>
+        {/* <SimpleBar style={{ height: '80vh' }}> */}
         {children}
+        {/* </SimpleBar> */}
       </Main>
       <Footer />
     </>
